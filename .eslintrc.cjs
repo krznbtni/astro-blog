594ef7308c976c42e0e2201c09215ad63f68f03d
Karzan Botani <karzan@botani.nu>
2023-01-14 14:03:58 +0100

    Allow null-assertions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
       extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'],
       rules: {
         '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_'}],
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
     {
