f3cf58c6b523f039558b75a785eb75769ccf59b4
Karzan Botani <karzan@botani.nu>
2023-01-14 14:07:25 +0100

    Add global MarkdownFrontmatter type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+interface MarkdownFrontmatter {
+  pubDate: string;
+  title: string;
+  description: string;
+  author: string;
+  image: {
+    url: string;
+    alt: string;
+  };
+  tags: Array<string>;
+}
